9e5a538a7839237ae11724d31425c6432d9272fc
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2023-11-21 00:44:09 +0300

    Add files via upload

---
+++ IOS/Optionals/Optionals/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Optionals
+//
+//  Created by Davud Gündüz on 21.11.2023.
+//
+
+import Foundation
+
+var myName : String!
+
+myName!.uppercased()
+
+var myAge = "d"
+
+if let myNumber = Int(myAge){
+    print(myNumber * 5)
+}else{
+    print("Wrong Input !!!")
+}
+
+
+
