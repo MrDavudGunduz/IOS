e4ad4c2242165d72de6558791fb97e7ef045ea62
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2023-12-26 12:02:27 +0300

    Add files via upload

--- IOS/FirebaseInstaClone/FirebaseInstaClone/ViewController.swift
+++ IOS/FirebaseInstaClone/FirebaseInstaClone/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Davud Gündüz on 25.12.2023.
 //
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -22,6 +23,27 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "toFeedVC", sender: nil)
     }
     @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authData, error in
+                if error != nil{
+                    self.makeAlert(titleInput: "Error!!!", messageInput: error!.localizedDescription)
+                }else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }else
+        {
+            makeAlert(titleInput: "Error", messageInput: "Username/Password?")
+        }
+        
+    }
+    
+    func makeAlert(titleInput : String , messageInput : String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
     
