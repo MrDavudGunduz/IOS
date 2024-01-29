7f0c4f6142d034ea318cf72e0c6a7dfdef0bc2ef
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2024-01-29 06:23:57 +0300

    Add files via upload

--- IOS/InstagramClone/InstagramClone/UploadViewController.swift
+++ IOS/InstagramClone/InstagramClone/UploadViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textField: UITextField!
@@ -16,9 +16,23 @@ class UploadViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UIGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
+    @objc func chooseImage(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    
+       func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           imageView.image = info[.originalImage] as? UIImage
+           self.dismiss(animated: true, completion: nil)
+       }
 
    
     @IBAction func saveClicked(_ sender: Any) {
