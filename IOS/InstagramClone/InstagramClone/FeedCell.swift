e519c52d2fe8143cd9c596ed5b21f66c2482c3a0
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2024-02-07 12:55:06 +0300

    Add files via upload

---
+++ IOS/InstagramClone/InstagramClone/FeedCell.swift
@@ -0,0 +1,34 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Davud Gündüz on 7.02.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var userEmailLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func likeButton(_ sender: Any) {
+    }
+}
