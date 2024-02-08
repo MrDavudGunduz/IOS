fc73e888cec834562577d2dce7370d95e378ec3a
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2024-02-08 18:12:26 +0300

    Add files via upload

---
+++ IOS/FoursquareCloneParse/FoursquareCloneParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareCloneParse
+//
+//  Created by Davud Gündüz on 8.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
