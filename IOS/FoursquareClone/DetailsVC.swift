618525574a569549744eb3b539848e79c441fcee
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2024-03-02 16:14:19 +0300

    Add files via upload

--- IOS/FoursquareClone/DetailsVC.swift
+++ IOS/FoursquareClone/DetailsVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MapKit
+import Parse
 
 class DetailsVC: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
@@ -21,12 +22,47 @@ class DetailsVC: UIViewController {
     
     @IBOutlet weak var mapKit: MKMapView!
     
+    var ChoosenPlacesID = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let query = PFQuery(className: "Places")
+        query.whereKey("objectsId", equalTo: ChoosenPlacesID)
+        query.findObjectsInBackground { objects, error in
+            if error != nil {
+                self.makeAlert(title: "Error!", message: error?.localizedDescription ?? "Error!")
+            }else
+            {
+                if objects != nil {
+                    let choosenPlacesObj = objects![0]
+                    
+                    if let placeName = choosenPlacesObj.object(forKey: "name") as? String{
+                        self.placeNameLabel.text! = placeName
+                        
+                        if let placeAtmosphere = choosenPlacesObj.object(forKey: "atmosphere") as? String{
+                            self.placeAtmospherLabel.text! = placeAtmosphere
+                            
+                            if let placeType = choosenPlacesObj.object(forKey: "type") as? String{
+                                self.placesTypeLabel.text! = placeType
+                                
+                                if let placesComment = choosenPlacesObj.object(forKey: "comment") as? String{
+                                    self.placesCommentLabel.text! = placesComment
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
-
-
+    func makeAlert(title : String , message : String){
+        let alert = UIAlertController(title: title , message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
--- IOS/FoursquareClone/PlacesVC.swift
+++ IOS/FoursquareClone/PlacesVC.swift
@@ -8,9 +8,15 @@
 import UIKit
 import Parse
 
-class PlacesVC: UIViewController {
-
+class PlacesVC: UIViewController , UITableViewDelegate , UITableViewDataSource{
+    
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var placesNameArray = [String]()
+    var placesIDArray = [String]()
+    var selectedPlacesID = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +24,37 @@ class PlacesVC: UIViewController {
         
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.done, target: self, action: #selector(logoutButtonClicked))
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        getDataFromParse()
+    }
+    
+    func getDataFromParse(){
+        let query = PFQuery(className: "Places")
+        query.findObjectsInBackground { objects, error in
+            if error != nil {
+                self.makeAlert(title: "Error!", message: error?.localizedDescription ?? "Error!")
+            }else{
+                if objects != nil {
+                    
+                    self.placesNameArray.removeAll(keepingCapacity: false)
+                    self.placesIDArray.removeAll(keepingCapacity: false)
+                    
+                    for object in objects! {
+                        if let placeName = object.object(forKey: "name") as? String {
+                            if let placeID = object.objectId{
+                                
+                                self.placesNameArray.append(placeName)
+                                self.placesIDArray.append(placeID)
+                                
+                            }
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     
 
@@ -38,6 +75,30 @@ class PlacesVC: UIViewController {
             }
         }
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destination = segue.destination as! DetailsVC
+            
+            destination.ChoosenPlacesID = selectedPlacesID
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPlacesID = placesIDArray[indexPath.row]
+        self.performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placesNameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = placesNameArray[indexPath.row]
+        return cell
     }
     
     func makeAlert(title : String , message : String){
