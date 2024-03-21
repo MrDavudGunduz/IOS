5e1222025601e8c9912ce744a1754653e4a00369
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2024-03-21 13:30:44 +0300

    I created the list
    I created the lists. I defined the elements. I created an array called myFavorites and transferred the models I defined to the array.

--- IOS/FavoriteBook/FavoriteBook/ContentView.swift
+++ IOS/FavoriteBook/FavoriteBook/ContentView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        List{
+            ForEach(myFavorites) { favorite in
+                Section(header: Text(favorite.title)) {
+                    ForEach(favorite.elements) { elemet in Text(elemet.name)
+        
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
-}
+
 
 #Preview {
     ContentView()
