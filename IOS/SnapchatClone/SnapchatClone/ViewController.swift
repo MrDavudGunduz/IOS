e61a9e778121725764a5c8ac1337f6ea7aa40dcf
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2024-03-23 23:48:38 +0300

    I created the snapchat cloning project

---
+++ IOS/SnapchatClone/SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Davud Gündüz on 23.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
