d6e7798eaed97eeef996cc05d78389fd09a6473b
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2024-10-12 18:13:00 +0300

    I added gesture recognizer and perform segue

---
+++ IAmPoor/IAmPoor/PoorManDetails.swift
@@ -0,0 +1,29 @@
+//
+//  PoorManDetails.swift
+//  IAmPoor
+//
+//  Created by Davud Gündüz on 12.10.2024.
+//
+
+import UIKit
+
+class PoorManDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- IAmPoor/IAmPoor/ViewController.swift
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var stoneView: UIImageView!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        stoneView.isUserInteractionEnabled = true
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        
+        stoneView.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        performSegue(withIdentifier: "toPoorManDetails", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSectoPoorManDetailsondVC" {
+            // as -- casting
+            let destinationVC = segue.destination as! PoorManDetails
+            
+        }
     }
 
 
